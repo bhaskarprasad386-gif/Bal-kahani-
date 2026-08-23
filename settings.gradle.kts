2dab285d5f2fdbce4b139b1ab3bcbd48dcb474e5
bhaskarprasad386-gif <bhaskarprasad386@gmail.com>
2026-08-23 22:11:08 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "minimal-android-project"
+include(":app")
